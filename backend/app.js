7ccdaaea2c6e4bd9e791acee606a26b3ed4639e9
adhamElhabashy <adhammohamed8321@gmail.com>
2024-05-24 17:35:06 +0300

    feature: add express.json and express-mongo-sanitize middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const sanitize = require("express-mongo-sanitize");
 const app = express();
+
+app.use(express.json());
+app.use(sanitize);
 
 module.exports = app;
