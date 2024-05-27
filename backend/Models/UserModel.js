fdb49891e55629a228f13b90eaaab26109ee1fc9
adhamElhabashy <adhammohamed8321@gmail.com>
2024-05-27 16:00:43 +0300

    feature: create User model

---
+++ backend/Models/UserModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+	username: { type: String, required: [true, "please enter your username"] },
+	email: {
+		type: String,
+		lowercase: true,
+		required: [true, "Please enter your email"],
+	},
+	password: {
+		type: String,
+		minlength: 8,
+		required: [true, "please enter your password"],
+		select: false,
+	},
+	confirmPassword: {
+		type: String,
+		required: [true, "please enter your password"],
+	},
+	profilePhoto: { type: String, default: "https://placehold.co/400" },
+	bio: { type: String, minlength: 10 },
+	accountVerified: { type: Boolean },
+	passwordChangedAt: Date,
+	passwordResetToken: String,
+	passwordResetTokenExpires: Date,
+});
+
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+
+	this.password = await bcrypt.hash(this.password, 12);
+
+	this.confirmPassword = undefined;
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
