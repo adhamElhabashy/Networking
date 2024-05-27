1fe25b9cf57abc88b462d50fd552cbe0bdd8ac11
adhamElhabashy <adhammohamed8321@gmail.com>
2024-05-27 14:46:28 +0300

    feature: connect to the db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
+mongoose.connect(process.env.CONN_STR).then(() => {
+	console.log("app connected to the db");
+});
+
 const port = process.env.PORT || 8000;
 
-const server = app.listen(5000, () => {
+const server = app.listen(port, () => {
 	console.log("server is running");
 });
