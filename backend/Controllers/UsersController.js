3a65eea7a6ce5f59379c21c8e7ade456adf4d665
adhamElhabashy <adhammohamed8321@gmail.com>
2024-05-29 18:40:04 +0300

    feature: complete get all users route

--- backend/Controllers/UsersController.js
+++ backend/Controllers/UsersController.js
@@ -7,4 +7,8 @@ const asyncHandler = require("express-async-handler");
 * @method GET
 * @access private - only admin
 -----------------------------------*/
-module.exports.getAllUsers = asyncHandler(async (request, response) => {});
+module.exports.getAllUsers = asyncHandler(async (request, response) => {
+	const users = await User.find();
+
+	response.status(200).json({ status: "success", data: { users } });
+});
