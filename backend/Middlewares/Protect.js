883aab2da055a55630bb1691cc1b90e290e096c3
adhamElhabashy <adhammohamed8321@gmail.com>
2024-06-09 17:55:19 +0300

    feature: add protect middleware to check user existence

--- backend/Middlewares/Protect.js
+++ backend/Middlewares/Protect.js
@@ -2,11 +2,11 @@ const asyncHandler = require("express-async-handler");
 const User = require("../Models/UserModel");
 
 Protect = asyncHandler(async (request, response, next) => {
-	const { id } = request.params;
+	const id = request.user.id;
 
-	const user = User.findOne({ _id: id });
+	const user = await User.findOne({ _id: id });
 
-	if (!user) {
+	if (!user || user.active !== true) {
 		return response
 			.status(404)
 			.json({ status: "fail", message: "user doesn't exist" });
