48f642a55e67fe478bba66dd5e7d79014c8641c7
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-11 17:12:56 +0300

    feature: create the top app bar

---
+++ frontend/src/Components/AppBar/AppBar.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ButtonComp from "../ButtonComp/ButtonComp";
+
+export default function AppBarComp() {
+	return (
+		<Box sx={{ flexGrow: 1, marginBottom: 0 }}>
+			<AppBar position="fixed">
+				<Toolbar>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						Networking
+					</Typography>
+					<ButtonComp
+						variant="contained"
+						size="large"
+						sx={{ marginRight: "10px" }}
+					>
+						Sign In
+					</ButtonComp>
+					<ButtonComp
+						variant="contained"
+						size="large"
+						sx={{ marginRight: "20px" }}
+					>
+						Sign Up
+					</ButtonComp>
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}
+					>
+						<MenuIcon fontSize="large" />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+}
