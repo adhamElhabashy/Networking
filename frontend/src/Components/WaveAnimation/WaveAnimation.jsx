b8e4b58b62d3fa1b12650de89ab13c2ff3c51296
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-11 17:13:40 +0300

    feature: create animated wave background

---
+++ frontend/src/Components/WaveAnimation/WaveAnimation.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./WaveAnimation.css";
+
+export default function WaveAnimation() {
+	return (
+		<div className="wave-container">
+			<div className="wave wave1"></div>
+			<div className="wave wave2"></div>
+			<div className="wave wave3"></div>
+		</div>
+	);
+}
