2c657fb410b9d1b72df9a59cd425546a618fce8c
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-11 18:39:24 +0300

    feature: add another section to the landing

--- frontend/src/Sections/Landing/Landing.jsx
+++ frontend/src/Sections/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import WaveAnimation from "../../Components/WaveAnimation/WaveAnimation";
-import { Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import Collaboration from "../../assets/Collaboration.png";
+import "./Landing.css";
 
 export default function Landing() {
 	return (
-		<>
+		<Box>
 			<WaveAnimation />
 			<Typography
 				sx={{
@@ -18,6 +20,26 @@ export default function Landing() {
 			>
 				Network, Collaborate, And Have Fun
 			</Typography>
-		</>
+			<Grid container sx={{ height: "100vh" }}>
+				<Grid item xs={12} lg={6} sx={{ position: "relative" }}>
+					<div className="text-box">
+						<Typography
+							sx={{
+								color: "primary.main",
+								flexGrow: 1,
+							}}
+							variant="h4"
+						>
+							Share your knowledge and get the knowledge from others
+						</Typography>
+					</div>
+				</Grid>
+				<Grid item xs={12} lg={6}>
+					<div className="img-holder">
+						<img src={Collaboration} alt="Collaboration Image" />
+					</div>
+				</Grid>
+			</Grid>
+		</Box>
 	);
 }
