bbca9d3e3c56af82bb3f8d9583bad458b184bd26
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-16 16:58:56 +0300

    feature: setup cors origin

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,16 @@ const AuthRouter = require("./Routes/AuthRoute");
 const UsersRouter = require("./Routes/UsersRoute");
 const PostsRouter = require("./Routes/PostsRoute");
 const CommentsRouter = require("./Routes/CommentsRoute");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(sanitize());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+	})
+);
 
 app.use("/api/v1/auth", AuthRouter);
 app.use("/api/v1/users", UsersRouter);
