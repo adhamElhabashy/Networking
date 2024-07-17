29282a10ed70650374cb822103eaa4c114795f9d
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-17 19:39:37 +0300

    fix: remove verifing the admin to get all the posts

--- backend/Routes/PostsRoute.js
+++ backend/Routes/PostsRoute.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const PostsController = require("../Controllers/PostsController");
 const { verifyToken } = require("../Middlewares/VerifyToken");
-const { verifyAdmin } = require("../Middlewares/VerifyAdmin");
 const { Protect } = require("../Middlewares/Protect");
 const { verifyUserPost } = require("../Middlewares/verifyUserPost");
 
 router
 	.route("/")
-	.get(verifyToken, Protect, verifyAdmin, PostsController.getAllThePosts)
+	.get(verifyToken, Protect, PostsController.getAllThePosts)
 	.post(verifyToken, PostsController.createPost);
 
 router
