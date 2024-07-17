82ce49d9445b99084bd4949e127b1dda405caa67
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-17 19:41:23 +0300

    feature: add get all posts api

---
+++ frontend/src/Api/PostsAPi/GetAllPosts.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+export default async function GetAllPosts() {
+	try {
+		const response = await axios.get(
+			`${import.meta.env.VITE_API_URL}/api/v1/posts`,
+			{
+				withCredentials: true,
+			}
+		);
+		const { data } = response.data;
+		return data;
+	} catch (error) {
+		const status = error.response ? error.response.status : null;
+		const message =
+			error.response && error.response.data
+				? error.response.data.message
+				: error.message;
+		throw { status, message };
+	}
+}
