3375fd1425d03be99f00bf9080a3a8e395d4bafe
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-20 19:20:48 +0300

    feature: add class name to post component

---
+++ frontend/src/Components/Post/Post.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+export default function Post({ post, className }) {
+	return (
+		<Card
+			sx={{ width: 345, marginTop: "10px" }}
+			key={post._id}
+			className={className}
+		>
+			<CardActionArea>
+				<CardMedia
+					component="img"
+					height="140"
+					image="/static/images/cards/contemplative-reptile.jpg"
+					alt="green iguana"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						{post.title}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						{post?.description?.slice(0, 100)}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
+}
