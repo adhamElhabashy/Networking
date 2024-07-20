ff23e7bae19ceda9e16a9f530896441c6678e64c
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-20 19:17:45 +0300

    feature: add get posts of user api call

---
+++ frontend/src/Api/UsersApi/GetPostsOfUser.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+export default async function GetPostsOfUser(id) {
+	try {
+		const response = await axios.get(
+			`${
+				import.meta.env.VITE_API_URL
+			}/api/v1/users/6665bb21a60fcac0182da359/posts`,
+			{
+				withCredentials: true,
+			}
+		);
+		const { data } = response.data;
+		return data;
+	} catch (error) {
+		const status = error.response ? error.response.status : null;
+		const message =
+			error.response && error.response.data
+				? error.response.data.message
+				: error.message;
+		throw { status, message };
+	}
+}
