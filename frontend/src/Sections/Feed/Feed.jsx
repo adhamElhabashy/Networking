1521354b57413a4b7a2d45008a7772719dff61ac
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-20 19:21:34 +0300

    feature: add class name to post component

--- frontend/src/Sections/Feed/Feed.jsx
+++ frontend/src/Sections/Feed/Feed.jsx
@@ -2,12 +2,8 @@ import * as React from "react";
 import "./Feed.css";
 import { Box } from "@mui/material";
 import GetAllPosts from "../../Api/PostsAPi/GetAllPosts";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import Post from "../../Components/Post/Post";
 
 export default function Feed() {
 	const [data, setData] = React.useState([]);
@@ -19,6 +15,7 @@ export default function Feed() {
 				setData(posts);
 			} catch (error) {
 				if (error.status === 401) {
+					window.localStorage.clear();
 					navigate("/signin");
 				}
 			}
@@ -26,28 +23,10 @@ export default function Feed() {
 		fetchData();
 	}, []); // Empty dependency array means this runs once after the first render
 
-	console.log(data.posts);
 	return (
 		<Box className="feed-box">
 			{data.posts?.map((post) => (
-				<Card sx={{ width: 345, marginTop: "10px" }} key={post._id}>
-					<CardActionArea>
-						<CardMedia
-							component="img"
-							height="140"
-							image="/static/images/cards/contemplative-reptile.jpg"
-							alt="green iguana"
-						/>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="div">
-								{post.title}
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
-								{post.description.slice(0, 100)}
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-				</Card>
+				<Post post={post} key={post._id} className={"feed-post"} />
 			))}
 		</Box>
 	);
