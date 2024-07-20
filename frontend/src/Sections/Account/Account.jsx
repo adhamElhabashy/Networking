0d28316365fd0610fce57939860d70087e510027
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-20 19:18:15 +0300

    feature: complete the account section

--- frontend/src/Sections/Account/Account.jsx
+++ frontend/src/Sections/Account/Account.jsx
@@ -1,33 +1,74 @@
 import * as React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import "./Account.css";
 import { useNavigate } from "react-router-dom";
+import GetPostsOfUser from "../../Api/UsersApi/GetPostsOfUser";
+import Post from "../../Components/Post/Post";
 
 export default function Account() {
 	const profile = JSON.parse(window.localStorage.getItem("profile"));
 	const navigate = useNavigate();
+	const [posts, setPosts] = React.useState([]);
+
 	React.useEffect(() => {
 		if (!profile) {
 			navigate("/signin");
 		} else {
-			console.log(profile);
+			const fetchData = async () => {
+				try {
+					const { posts } = await GetPostsOfUser(profile.user._id);
+					setPosts(posts);
+				} catch ({ status, message }) {
+					if (status === 401) {
+						window.localStorage.clear();
+						navigate("/signin");
+					}
+				}
+			};
+
+			fetchData();
 		}
 	}, []);
+
 	return (
 		<Box className="account-container">
-			<div className="account-info">
-				<Grid container>
-					<Grid item>
-						<div className="img-holder">
-							<img src={profile.user.profilePhoto} alt="profile photo" />
-						</div>
+			<Container>
+				<Box
+					className="account-info"
+					sx={{
+						flexDirection: { xs: "column", md: "row" },
+						textAlign: { xs: "center", md: "left" },
+					}}
+				>
+					<div className="image-holder">
+						<img src={profile?.user?.profilePhoto} alt="profile photo" />
+					</div>
+					<div className="user-info">
+						<Typography variant="h5" color={"secondary"} className="user-info">
+							{profile?.user?.username}
+						</Typography>
+						<Typography
+							variant="subtitle1"
+							color={"secondary"}
+							className="user-info"
+						>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+							adipisci corporis id quae dignissimos nam pariatur, inventore odio
+							labore incidunt, quos accusamus nesciunt perspiciatis dolorum
+							cupiditate quod. At, praesentium nihil.
+						</Typography>
+					</div>
+				</Box>
+				<div className="posts-box">
+					<Grid container spacing={2}>
+						{posts?.map((post) => (
+							<Grid item xs={12} md={6} lg={4} key={post._id} className="post">
+								<Post post={post} className="account-post" />
+							</Grid>
+						))}
 					</Grid>
-					<Grid item>
-						<Typography variant="h5">{profile.user.username}</Typography>
-						<Typography variant="h6">{profile.user.bio}</Typography>
-					</Grid>
-				</Grid>
-			</div>
+				</div>
+			</Container>
 		</Box>
 	);
 }
