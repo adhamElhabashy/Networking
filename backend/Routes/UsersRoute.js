5a1958685c87b98af3099efd651ebc5b53a3b4a1
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-20 15:27:43 +0300

    feature: add protect middleware to users route

--- backend/Routes/UsersRoute.js
+++ backend/Routes/UsersRoute.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const UsersController = require("../Controllers/UsersController");
 const { verifyToken } = require("../Middlewares/VerifyToken");
 const { verifyAdmin } = require("../Middlewares/VerifyAdmin");
+const { Protect } = require("../Middlewares/Protect");
 
 router.route("/").get(verifyToken, verifyAdmin, UsersController.getAllUsers);
 router
 	.route("/:id")
-	.put(verifyToken, UsersController.UpdateMyProfile)
-	.delete(verifyToken, UsersController.deleteUserProfile);
-
+	.put(verifyToken, Protect, UsersController.UpdateMyProfile)
+	.delete(verifyToken, Protect, UsersController.deleteUserProfile);
 module.exports = router;
