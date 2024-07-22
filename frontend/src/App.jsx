d7da9eb482518d37a6bcc36fb3c89f76500d8596
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-22 15:20:41 +0300

    feature: add createPostPage component to routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import SignIn from "./Sections/SignIn/SignIn";
 import Feed from "./Sections/Feed/Feed";
 import Account from "./Sections/Account/Account";
 import PostPage from "./Sections/PostPage/PostPage";
+import CreatePostPage from "./Sections/CreatePostPage/CreatePostPage";
 
 let theme = createTheme({
 	palette: { primary: { main: "#0099FF" }, secondary: { main: "#fff" } },
@@ -26,6 +27,7 @@ function App() {
 				<Route path="/signin" element={<SignIn />} />
 				<Route path="/feed" element={<Feed />} />
 				<Route path="/account" element={<Account />} />
+				<Route path="/account/createpost" element={<CreatePostPage />} />
 				<Route path="/posts/:id" element={<PostPage />} />
 			</Routes>
 		</ThemeProvider>
