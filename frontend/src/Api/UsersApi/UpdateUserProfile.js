6c7e2b24df36bed7692af27cad84ad9ae951c8be
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-27 15:48:18 +0300

    feature add update user profile call

---
+++ frontend/src/Api/UsersApi/UpdateUserProfile.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+export default async function UpdateUserProfile(id, email, username, bio) {
+	try {
+		const response = await axios.put(
+			`${import.meta.env.VITE_API_URL}/api/v1/users/${id}`,
+			{
+				email,
+				username,
+				bio,
+			},
+			{
+				withCredentials: true,
+			}
+		);
+		const { data } = response;
+		return data;
+	} catch (error) {
+		console.error("Error details:", error); // Log full error details
+		if (error.response) {
+			console.error("Response data:", error.response.data);
+			console.error("Response status:", error.response.status);
+			console.error("Response headers:", error.response.headers);
+		} else if (error.request) {
+			console.error("Request data:", error.request);
+		} else {
+			console.error("Error message:", error.message);
+		}
+		throw error;
+	}
+}
