f09c893b1734ac530b6de331b8d01a9d5e15136b
adhamElhabashy <adhammohamed8321@gmail.com>
2024-07-27 16:36:31 +0300

    feature: add delete user account feature

--- frontend/src/Sections/Account/Account.jsx
+++ frontend/src/Sections/Account/Account.jsx
@@ -6,11 +6,27 @@ import { Link, useNavigate } from "react-router-dom";
 import GetPostsOfUser from "../../Api/UsersApi/GetPostsOfUser";
 import Post from "../../Components/Post/Post";
 import UpdateUserForm from "../../Components/UpdateUserForm/UpdateUserForm";
+import DeleteUser from "../../Api/UsersApi/DeleteUser";
+import removeFromLocalStorage from "../../Functions/RemoveFromLocalStorage";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function Account() {
 	const profile = JSON.parse(window.localStorage.getItem("profile"));
 	const navigate = useNavigate();
 	const [posts, setPosts] = React.useState([]);
+	const [open, setOpen] = React.useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
 
 	React.useEffect(() => {
 		if (!profile) {
@@ -32,6 +48,14 @@ export default function Account() {
 		}
 		// this feature will be replaced with another technology
 	}, [GetPostsOfUser(profile.user._id)]);
+
+	async function callDeleteUser() {
+		handleClose();
+		const response = await DeleteUser(profile.user._id);
+		removeFromLocalStorage();
+
+		navigate("/");
+	}
 
 	return (
 		<Box className="account-container" sx={{ backgroundColor: "primary.main" }}>
@@ -58,6 +82,36 @@ export default function Account() {
 							{profile?.user?.bio}
 						</Typography>
 						<UpdateUserForm />
+						<Button
+							variant="contained"
+							color="error"
+							className="delete-account-btn"
+							onClick={handleClickOpen}
+						>
+							Delete Account
+						</Button>
+						<Dialog
+							open={open}
+							onClose={handleClose}
+							aria-labelledby="alert-dialog-title"
+							aria-describedby="alert-dialog-description"
+						>
+							<DialogTitle id="alert-dialog-title">
+								Confirm Deleting Your Account
+							</DialogTitle>
+							<DialogContent>
+								<DialogContentText id="alert-dialog-description">
+									If you still want to delete your account click "Delete My
+									Account"
+								</DialogContentText>
+							</DialogContent>
+							<DialogActions>
+								<Button onClick={handleClose}>Cancel</Button>
+								<Button onClick={callDeleteUser} autoFocus>
+									Delete My Account
+								</Button>
+							</DialogActions>
+						</Dialog>
 					</div>
 				</Box>
 				<div className="posts-box">
