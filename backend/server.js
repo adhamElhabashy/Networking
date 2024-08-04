765cb5b58ea579d606710aa55389d8084aec33dc
adhamElhabashy <adhammohamed8321@gmail.com>
2024-08-04 15:07:01 +0300

    feature: handle uncaughtException

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
+process.on("uncaughtException", (err) => {
+	console.log(err.name, err.message);
+	console.log("Uncaught Exception occured! Shutting down...");
+	process.exit(1);
+});
+
 const app = require("./app");
 
 mongoose.connect(process.env.CONN_STR).then(() => {
